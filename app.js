2c9bd55c93071a8fab7a0392acbf37dc37f97f82
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-05-03 23:08:38 +0530

    Q9-CleanUpCode&Exports

--- app.js
+++ app.js
@@ -1,44 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    
-    const url = req.url;
-    const method = req.method;
+const routes = require('./routes');
 
-    if (url === '/'){
-        res.write('<html>');
-        const fileData = fs.readFileSync('./message.txt', 'utf8');
-        res.write(fileData);
-        res.write('<head><title>Custom Response</title></head>');
-        res.write('<body><form action="/mes" method="POST"><input type="text" name="message"></input><button type = "submit">SEND</button></form></body>')
-        res.write('</html>')
-        return res.end();
-    }   
-
-    if(url === '/mes' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunkOfData) => {
-            body.push(chunkOfData);
-        });
-        return req.on('end',()=> {
-            const parsedBody = Buffer.concat(body).toString();
-            let message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt',message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location','/');
-                res.write('<html><body> message here</body> </html>');
-                return res.end();
-            });
-        })
-    }
-
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<body>Welcome home</body>');
-    res.write('</html>');
-    res.end();
-    
-});
+const server = http.createServer(routes);
 
 server.listen(4000);
