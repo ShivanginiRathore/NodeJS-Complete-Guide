5735767aec845846f322934542a3477b4026f471
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-05-03 13:32:16 +0530

    Q7-CustomResponses

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit();
     res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<body>Shivangini Rathore</body>');
-    res.write('</html>');
-    res.end();
+    if (req.url == '/home'){
+        res.write('<html>');
+        res.write('<body>Welcome home</body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url == '/about'){
+        res.write('<html>');
+        res.write('<body>Welcome to About Us page</body>');
+        res.write('</html>');
+        res.end();
+    } else if(req.url == '/node'){
+        res.write('<html>');
+        res.write('<body>Welcome to my Node Js project</body>');
+        res.write('</html>');
+        res.end();
+    }
+    
 });
 
 server.listen(4000);
