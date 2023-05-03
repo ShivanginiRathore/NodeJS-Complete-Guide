51424e538b9a7e075ef1aedaae9fa74c1eb6e3b8
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-05-03 15:08:56 +0530

    Q8-ReadWritefromFile

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-    res.setHeader('Content-Type','text/html');
-    if (req.url == '/home'){
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/'){
         res.write('<html>');
-        res.write('<body>Welcome home</body>');
-        res.write('</html>');
-        res.end();
-    } else if (req.url == '/about'){
-        res.write('<html>');
-        res.write('<body>Welcome to About Us page</body>');
-        res.write('</html>');
-        res.end();
-    } else if(req.url == '/node'){
-        res.write('<html>');
-        res.write('<body>Welcome to my Node Js project</body>');
-        res.write('</html>');
-        res.end();
+        const fileData = fs.readFileSync('./message.txt', 'utf8');
+        res.write(fileData);
+        res.write('<head><title>Custom Response</title></head>');
+        res.write('<body><form action="/mes" method="POST"><input type="text" name="message"></input><button type = "submit">SEND</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }   
+
+    if(url === '/mes' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunkOfData) => {
+            body.push(chunkOfData);
+        });
+        return req.on('end',()=> {
+            const parsedBody = Buffer.concat(body).toString();
+            let message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt',message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                res.write('<html><body> message here</body> </html>');
+                return res.end();
+            });
+        })
     }
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<body>Welcome home</body>');
+    res.write('</html>');
+    res.end();
     
 });
 
