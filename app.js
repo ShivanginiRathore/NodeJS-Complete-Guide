fe01ef81f0e577fa243d32e3cc36d558fe047165
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-05-10 14:15:53 +0530

    Q3_Routers_Filters

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express(); 
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><input type="text" name="size"></input><button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-// for only incoming get/post request
-app.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1> Hello from express!</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page not found!</h1>');
 });
 
 app.listen(4001);
